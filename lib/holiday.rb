05d8553b63c1bfa523efac3d8d5f2654834eaeaa
Michael Pope <popemray1986@gmail.com>
2017-10-29 18:18:21 +0000

    Done.

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -22,7 +22,8 @@ def second_supply_for_fourth_of_july(holiday_hash)
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
-  holiday_hash[:winter][:christmas][:new_years] << supply
+  holiday_hash[:winter][:christmas] << "Balloons"
+  holiday_hash[:winter][:new_years] << "Balloons"
   # holiday_hash is identical to the one above
   # add the second argument, which is a supply, to BOTH the
   # Christmas AND the New Year's arrays
@@ -31,21 +32,26 @@ end
 
 
 def add_supply_to_memorial_day(holiday_hash, supply)
+  holiday_hash[:spring][:memorial_day] << "#{supply}"
   # again, holiday_hash is the same as the ones above
   # add the second argument to the memorial day array
 
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
-  # code here
-  # remember to return the updated hash
+  holiday_hash[season] = {holiday_name => supply_array}
 
+
+  # remember to return the updated hash
+holiday_hash
 end
 
 def all_winter_holiday_supplies(holiday_hash)
+  holiday_hash[:winter].values.flatten
   # return an array of all of the supplies that are used in the winter season
-
 end
+
+
 
 def all_supplies_in_holidays(holiday_hash)
   # iterate through holiday_hash and print items such that your readout resembles:
@@ -55,11 +61,21 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
+  holiday_hash.each do |season, holidays|
+    puts "#{season.capitalize}:"
+      holidays.each do |holiday, supplys|
+        puts "  #{holiday.to_s.split("_").map{|i| i.capitalize}.join(" ")}: #{supplys.join(", ")}"
+      end
 
+  end
 end
 
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-
+  holiday_hash.map do |season, holiday|
+    holiday.map do |holiday, item|
+      holiday if item.include?("BBQ")
+    end
+  end.flatten.compact
 end
